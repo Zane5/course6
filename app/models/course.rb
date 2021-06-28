cb9e713800a9601bd6c3d90c5b2c6433becc0136
vagrant <vagrant@x.dev>
2021-06-28 05:38:05 +0000

    add action_text

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,8 @@ class Course < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true, length: { minimun: 3, maximum: 500 }
 
+  has_rich_text :description
+
   def to_s
     title
   end
