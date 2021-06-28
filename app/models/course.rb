75082d5cd4e761f29219476489d0c38857722ec0
vagrant <vagrant@x.dev>
2021-06-28 09:08:00 +0000

    assicoate user and courese

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,7 @@ class Course < ApplicationRecord
   validates :description, presence: true, length: { minimun: 3, maximum: 500 }
 
   has_rich_text :description
+  belongs_to :user
 
   def to_s
     title
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create!(email: 'admin@e.com', password: 'eeeeee', password_confirmation: 'eeeeee')
+
 30.times do
   Course.create!([{
                    title: Faker::Educator.course_name,
-                   description: Faker::TvShows::GameOfThrones.quote
+                   description: Faker::TvShows::GameOfThrones.quote,
+                   user_id: User.first.id
                  }])
 end
